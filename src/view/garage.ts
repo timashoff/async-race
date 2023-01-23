9153ede751b7f0891c51047f23f28cc847b10872
Roman Timashoff <timashoff@gmail.com>
2023-01-23 21:42:09 +0600

    feat: implement page pagination element

--- src/view/garage.ts
+++ src/view/garage.ts
@@ -1,9 +1,10 @@
 import { createMenu } from './menu';
 import { createPageTitle } from './page-title';
+import { createPagination } from './page-pagination';
 
 export const createGarage = () => {
   const garage = document.createElement('div');
   garage.id = 'garage';
-  garage.append(createMenu(), createPageTitle('garage', 4));
+  garage.append(createMenu(), createPageTitle('garage', 4), createPagination(1, 2));
   return garage;
 };
---
+++ src/view/page-pagination.ts
@@ -0,0 +1,22 @@
+import { createBtn } from '../utils/create-buttons';
+import { createDiv } from '../utils/create-divs';
+
+export const createPagination = (firstPage: number, lastPage: number) => {
+
+  const pagination = createDiv('pagination');
+
+  const prev = createBtn('prev');
+  prev.textContent = '◀';
+  if (firstPage === 1) prev.disabled = true;
+
+  const next = createBtn('next');
+  next.textContent = '▶';
+  if (firstPage === lastPage) next.disabled = true;
+
+  const pageCounter = document.createElement('span');
+  pageCounter.classList.add('page-counter');
+  pageCounter.textContent = lastPage > 1 ? `${firstPage}/${lastPage}` : `${lastPage}`;
+
+  pagination.append(prev, pageCounter, next);
+  return pagination;
+};
